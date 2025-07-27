7938d97f1c98ee3ce4ad7d0baa9d5378196831cd
ananyaaa21 <132597794+ananyaaa21@users.noreply.github.com>
2025-07-27 15:05:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ if ingredients_list:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/all")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
